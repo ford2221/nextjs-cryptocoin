147dc2fd1b9236296952ea5440b5ab56ac1909d8
ford2221 <fordstelle@gmail.com>
2023-02-07 20:54:53 -0400

    last changes

--- pages/cardcoin.js
+++ pages/cardcoin.js
@@ -13,8 +13,8 @@ export default function CardCoin({ coins, search }) {
     <>
       <div className='pb-40 flex flex-wrap gap-3 justify-center px-5'>
         {filteredCoins?.map((coin) => (
-          <div  key={coin.id} className="w-60 h-auto bg-blue-900 rounded-lg text-xs hover:shadow hover:shadow-indigo-900 ">
-            <div  className="w-60 h-auto bg-gray-900 opacity-80 rounded-lg mt-1 p-2 hover:cursor-pointer ">
+          <div  key={coin.id} className="w-60 h-20 mb-8 bg-blue-900 rounded-lg text-xs hover:shadow hover:shadow-indigo-900 ">
+            <div style={{ background: '#02072F'}} className="w-60 h-auto  rounded-lg mt-1 p-2 hover:cursor-pointer ">
               <div className="flex gap-6 items-center">
                 <Image src={coin.image} alt="" width={25} height={25}/>
                 <h3 className="text-gray-400 text-xs">{coin.name} ( <span className="text-gray-300">{coin.symbol}</span> )</h3> 
@@ -26,7 +26,7 @@ export default function CardCoin({ coins, search }) {
               
               <div className="flex items-center justify-between  border-t-ingo-500 py-1">
                 <h4 className="text-gray-400">Price Change:</h4>
-                <p className={coin.price_change_percentage_24h > 0 ? "text-green-600 " : "text-red-600 "}>
+                <p className={coin.price_change_percentage_24h > 0 ? "text-green-700 " : "text-red-600 "}>
                   {coin.price_change_percentage_24h}
                 </p>
               </div>
